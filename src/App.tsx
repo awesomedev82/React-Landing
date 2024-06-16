542480581f5f55d3942042faf56310065a642503
awesomedev82 <awesomedev08@gmail.com>
2024-06-16 12:00:18 -0400

    iniitialize app

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="bg-blue-500 text-white p-5 text-xl">
+        Hello, React with TypeScript and Tailwind!
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Adjust this line to include TypeScript files
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
